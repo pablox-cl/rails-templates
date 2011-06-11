266224f9e298e23cfc0745cb1d6520758c342caf
Pablo Olmos de Aguilera <pablo@glatelier.org>
2011-06-11 18:11:32 -0400

    Fix gemset issue, now has colored output and appended more files to gitignore

--- simple.rb
+++ simple.rb
@@ -1,14 +1,24 @@
-# create a unique gemset
+# check prerequisites
 
+%w{colored bundler}.each do |component|
+  unless Gem::Specification::find_by_name(component)
+    run "gem install #{component}"
+    Gem::Specification.refresh
+    Gem::Specification.activate(component)
+  end
+  require "#{component}"
+end
+
+puts 'Starting simple template...'.magenta
+
+# create a unique gemset
 run "rvm gemset create #{app_name}"
-create_file ".rvmrc", "rvm gemset use #{app_name}"
+create_file ".rvmrc", "rvm use @#{app_name}"
 
 # install gems
 gem "haml", "~> 3.1.1"
 gem "sass", "~> 3.1.2"
 gem "nifty-generators", "~> 0.4.6", :group => :development
-
-run 'bundle install'
 
 generate 'nifty:layout --haml'
 remove_file 'app/views/layouts/application.html.erb'
@@ -18,18 +28,24 @@ generate 'nifty:config'
 remove_file 'public/index.html'
 remove_file 'rm public/images/rails.png'
 
-# git
-file '.gitignore', <<-END
-  .DS_Store
-  .bundle
-  db/*.sqlite3
-  log/*.log
-  tmp/
-  .sass-cache/
+# make sure the gems are installed on our gemset
+run "rvm @#{app_name} -S bundle install"
 
-  .project
-  config/database.yml
-  Gemfile.lock
+# git
+append_file '.gitignore', <<-END
+.DS_Store
+.project
+config/database.yml
+Gemfile.lock
+
+# netbeans project directory
+/nbproject/
+
+# textmate project files
+/*.tmpproj
+
+# vim
+**.swp
 END
 
 git :init
